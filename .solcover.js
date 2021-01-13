f72f136859e216a6468190957a57080ea16bb4c6
Mike <manhlx.300695@gmail.com>
2021-01-13 15:46:19 +0700

    Update default eth balance for coverage

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,6 @@
 module.exports = {
     providerOptions: {
-        default_balance_ether: 100000000000000,
+        default_balance_ether: 100000000000000000000000000000000,
         total_accounts: 20,
     },
     skipFiles: ['sol4/', 'sol6/mock/', 'sol6/utils/zeppelin/'],
